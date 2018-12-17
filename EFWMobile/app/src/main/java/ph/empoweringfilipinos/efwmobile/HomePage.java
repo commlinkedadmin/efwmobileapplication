dab1d38c94330705a867e6e473f26f4ff5e2b0ea
Vennise Maeka <Vennise Maeka@DESKTOP-PF7V2T8>
2018-12-17 16:07:22 +0800

    Second Commit

--- EFWMobile/app/src/main/java/ph/empoweringfilipinos/efwmobile/HomePage.java
+++ EFWMobile/app/src/main/java/ph/empoweringfilipinos/efwmobile/HomePage.java
@@ -1,11 +1,19 @@
 package ph.empoweringfilipinos.efwmobile;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.os.Message;
+import com.getbase.floatingactionbutton.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import com.github.clans.fab.FloatingActionMenu;
 
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
@@ -16,28 +24,89 @@ public class HomePage extends AppCompatActivity {
 
     int[] sampleImages = {R.drawable.image_1, R.drawable.image_2, R.drawable.image_3, R.drawable.image_4, R.drawable.image_5};
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
 
+
+
         carouselView = findViewById(R.id.carouselView);
         carouselView.setPageCount(sampleImages.length);
         carouselView.setImageListener(imageListener);
 
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
 
 
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        //FAB LINKS
+        com.github.clans.fab.FloatingActionButton fabNews=findViewById(R.id.fabNews);
+
+        fabNews.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+
+                //code here
+            }
+        });
+
+        com.github.clans.fab.FloatingActionButton fabBlogs=findViewById(R.id.fabBlogs);
+
+        fabBlogs.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+
+                //code here
+            }
+        });
+
+        com.github.clans.fab.FloatingActionButton fabCommunities=findViewById(R.id.fabCommunities);
+
+        fabCommunities.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+
+                //code here
+            }
+        });
+
+        com.github.clans.fab.FloatingActionButton fabAboutUs=findViewById(R.id.fabAboutUs);
+
+        fabAboutUs.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+
+                //code here
+                showToast("Test");
+            }
+        });
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+    public void showToast(String message){
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
 
